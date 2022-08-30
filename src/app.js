82557a3ba4017d535289bda611793acd11c46b93
DavidBolaji <odavidbolaji14@gmail.com>
2022-08-30 11:49:32 +0100

    fnrs

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+require('dotenv').config();
+
+// model start
+const app = express();
+const PORT = process.env.PORT || 5000
+const userRouter = require('./routes/user')
+
+const cors = require('cors')
+app.use(cors({
+    origin: '*'
+}))
+
+// app use
+
+app.use(express.json());
+app.use(userRouter);
+
+
+app.listen(PORT, () => {
+    console.log(`SERVER IS RUNNING ON PORT ${PORT}`);
+})
