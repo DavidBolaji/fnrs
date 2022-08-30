45d5415c9d0fc1488fc2caba5ddf8eaf9e9a242f
DavidBolaji <odavidbolaji14@gmail.com>
2022-08-30 13:13:52 +0100

    add product

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000
 const userRouter = require('./routes/user')
+const productRouter = require('./routes/product')
 
 const cors = require('cors')
 app.use(cors({
@@ -15,6 +16,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(userRouter);
+app.use(productRouter)
 
 
 app.listen(PORT, () => {
